d6ac1e3db184755a9cdb39e8ebc0e74a448477ef
Anatoly Tsybin <cybin94@mail.ru>
2020-03-12 20:45:37 +0300

    Updated Pascal's HelloWorld, add some arithmetic output.

--- Pascal/01-HelloWorld.pas
+++ Pascal/01-HelloWorld.pas
@@ -1,4 +1,8 @@
 program HelloWorld;
 begin
-	writeln ('Hello, World!')
+	writeln ('Hello, World!');
+	writeln ('Here is the result of addition of 32 and 128: ', 32+128);
+	writeln ('And the result of subtraction of the same numbers: ', 32-128);
+	writeln ('Multiplication: ', 32*128);
+	writeln ('Division: ', 32/128)
 end.
