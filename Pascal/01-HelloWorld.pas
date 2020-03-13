2e1a63ee8afe26bf2012b0fc73311eb727fc60aa
Anatoly Tsybin <cybin94@mail.ru>
2020-03-13 09:04:32 +0300

    Add integer division operators.

--- Pascal/01-HelloWorld.pas
+++ Pascal/01-HelloWorld.pas
@@ -4,5 +4,7 @@ begin
 	writeln ('Here is the result of addition of 32 and 128: ', 32+128);
 	writeln ('And the result of subtraction of the same numbers: ', 32-128);
 	writeln ('Multiplication: ', 32*128);
-	writeln ('Division: ', 32/128)
+	writeln ('Division: ', 32/128);
+	writeln ('Integer division of 127 by 32: ', 127 div 32);
+	writeln ('Remainder of integer division of 127 by 32: ', 127 mod 32)
 end.
