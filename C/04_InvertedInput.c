6715af93338cff30bdccba2ca570973572e65510
Anatoly Tsybin <cybin94@mail.ru>
2020-03-22 17:12:49 +0300

    Input/output moved to functions, no scanf().

--- C/04_InvertedInput.c
+++ C/04_InvertedInput.c
@@ -7,33 +7,48 @@ struct singly_linked_list
 	struct singly_linked_list *next;
 };
 
-int main()
+struct singly_linked_list *list_input()
 {
-	struct singly_linked_list *first = NULL, *tmp = NULL;
+	struct singly_linked_list *tmp = NULL, *current = NULL;
+	char buf[16];
 	int c;
 
-	printf("Enter as many integers as you want and Ctrl+D to finish:\n");
-	
-	while(scanf("%d", &c) != EOF){
-		if(first != NULL){
-			tmp = first;
-			first = malloc(sizeof(struct singly_linked_list));
-			first->element = c;
-			first->next = tmp;
-		} else {
-			first = malloc(sizeof(struct singly_linked_list));
-			first->element = c;
-		}
+	// FIXME: write input for processing not only line by line
+	while(fgets(buf, sizeof(buf), stdin)){
+		if(sscanf(buf, "%d", &c) == 1){
+			tmp = current;
+			current = malloc(sizeof(struct singly_linked_list));
+			current->element = c;
+			current->next = tmp;
+		} else
+			printf("Incorrect input, try again: \n");
 	}
 
-	printf("Entered numbers in inverted order is:\n");
+	return current;
+}
 
-	while(first != NULL){
-		printf("%d\n", first->element);
-		tmp = first;
-		first = first->next;
+void list_output(struct singly_linked_list *current)
+{
+	struct singly_linked_list *tmp = NULL;
+
+	while(current){
+		printf("%d ", current->element);
+		tmp = current;
+		current = current->next;
 		free(tmp);
 	}
+}
+
+int main()
+{
+	struct singly_linked_list *first = NULL;
+
+	printf("Enter as many integers as you want one per line and Ctrl+D to finish:\n");
+	first = list_input();
+
+	printf("Entered numbers in inverted order is:\n");
+	list_output(first);
+	putchar(10);
 
 	return 0;
 }
