8d83a1ab78ac7c827f1f501c1385ad25e5d8b089
Anatoly Tsybin <cybin94@mail.ru>
2020-04-07 18:25:21 +0300

    Add HelloWorld program in C++

---
+++ CPP/01_HelloWorld.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+int main()
+{
+	std::cout
+		<< "Hello, World!"
+		<< std::endl;
+	std::cout
+		<< "Addition of 32 and 128 is "
+		<< 32+128
+		<< std::endl;
+	std::cout
+		<< "Subtraction of the same numbers is "
+		<< 32-128
+		<< std::endl;
+	std::cout
+		<< "Multiplication result is "
+		<< 32*128
+		<< std::endl;
+	std::cout
+		<< "Division result is "
+		<< 32.0/128.0
+		<< std::endl;
+	std::cout
+		<< "Integer division of 127 by 32 result is "
+		<< 127/32
+		<< std::endl;
+	std::cout
+		<< "Remainder of integer division of the same numbers is "
+		<< 127%32
+		<< std::endl;
+
+	return 0;
+}
